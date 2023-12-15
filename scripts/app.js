9830f14bc269a59df278e63162bf33121ed03c9e
Nathan <antnefedov33@gmail.com>
2023-12-15 16:22:28 +0300

    made slider, wrote html and css for it, added parallax effect

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,19 @@
 "use strict"
+
+let sliderMain = new Swiper('.slider_main', {
+    freeMode: true,
+    centeredSlides: true,
+    mousewheel: true,
+    parallax: true,
+    breakpoints: {
+        0: {
+            slidesPerView: 2.5,
+            spaceBetween: 20,
+        },
+        680: {
+            slidesPerView: 3.5,
+            spaceBetween: 60,
+        }
+    }
+});
+
